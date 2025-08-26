e52ef3abedd855172d777243d7a9efd6b35404e0
AlbanMarku <albanmarku@outlook.com>
2025-08-26 13:40:48 +0100

    User prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,11 +9,17 @@ api_key  = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if len(sys.argv) < 2:
+        print("Usage: python main.py '<your prompt here>'")
+        sys.exit(1)
+    else:
+        userPrompt = sys.argv[1]
+    
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=userPrompt)
     print(response.text)
     print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
     print("Response tokens: " + str(response.usage_metadata.thoughts_token_count))
-
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
